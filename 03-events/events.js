eb85a7d6683abdce2ef74bce48d4d6850e852537
Tuahta Ramadhani <tuahtaramdhani@gmail.com>
2024-12-05 15:27:22 +0700

    memiliki fungsi emit() yang berguna untuk membangkitkan event

--- 03-events/events.js
+++ 03-events/events.js
@@ -3,9 +3,17 @@ const { EventEmitter } = require("events");
 const myEventEmitter = new EventEmitter();
 
 // fungsi yang akan dijalankan ketika event coffe-order terjadi
-const makeCoffe = ({ name }) => {
+const makeCoffee = ({ name }) => {
   console.log(`Kopi ${name} telah dibuat!`);
 };
 
 // mendaftarkan fungsi makeCoffe sebagai listener event coffe-order
-myEventEmitter.on("coffe-order", makeCoffe);
+myEventEmitter.on("coffee-order", makeCoffee);
+
+// Memicu event 'coffee-order' terjadi.
+myEventEmitter.emit("coffee-order", { name: "Tubruk" });
+
+/*
+ * output:
+ * Kopi Tubruk telah dibuat!
+ */
