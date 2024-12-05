712e6737f899c5380e8e0139a9aff3b0bc60854e
Tuahta Ramadhani <tuahtaramdhani@gmail.com>
2024-12-05 15:36:39 +0700

    tambahkan variabel makeBill dengan const dan price dengan nilai 15 ribu untuk mendaftarkan lebih dari satu fungsi listener pada sebuah event menggunakan fungsi on

--- 03-events/events.js
+++ 03-events/events.js
@@ -2,16 +2,25 @@ const { EventEmitter } = require("events");
 
 const myEventEmitter = new EventEmitter();
 
-// fungsi yang akan dijalankan ketika event coffe-order terjadi
+// fungsi yang akan dijalankan ketika event coffee-order terjadi
 const makeCoffee = ({ name }) => {
   console.log(`Kopi ${name} telah dibuat!`);
 };
 
-// mendaftarkan fungsi makeCoffe sebagai listener event coffe-order
+// tambahkan variabel makeBill dengan const
+const makeBill = ({ price }) => {
+  console.log(`Bill sebesar ${price} telah dibuat!`);
+};
+
+// mendaftarkan fungsi makeCoffe sebagai listener event coffee-order
 myEventEmitter.on("coffee-order", makeCoffee);
 
-// Memicu event 'coffee-order' terjadi.
-myEventEmitter.emit("coffee-order", { name: "Tubruk" });
+// tambahkan mendaftarkan fungsi makeBill sebagai listener event coffee-order
+myEventEmitter.on("coffee-order", makeBill);
+
+// Memicu event 'coffee-order' terjadi.\
+// tambahkan variabel price dengan nilai 15 ribu
+myEventEmitter.emit("coffee-order", { name: "Tubruk", price: 15000 });
 
 /*
  * output:
