3831450826432d184e6bff0cc41859d564c3aece
Tuahta Ramadhani <tuahtaramdhani@gmail.com>
2024-12-05 15:02:51 +0700

    setiap instance dari EventEmitter akan memiliki fungsi "on"

--- 03-events/events.js
+++ 03-events/events.js
@@ -1,3 +1,11 @@
 const { EventEmitter } = require("events");
 
 const myEventEmitter = new EventEmitter();
+
+// fungsi yang akan dijalankan ketika event coffe-order terjadi
+const makeCoffe = ({ name }) => {
+  console.log(`Kopi ${name} telah dibuat!`);
+};
+
+// mendaftarkan fungsi makeCoffe sebagai listener event coffe-order
+myEventEmitter.on("coffe-order", makeCoffe);
