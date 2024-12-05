1af9d3fb56cd101d23df8b7dd9b422d09cfb0520
Tuahta Ramadhani <tuahtaramdhani@gmail.com>
2024-12-05 16:08:34 +0700

    Atau bisa membuat satu fungsi khusus untuk menangani event. Biasanya fungsi ini memiliki nama ‘handler’ atau ‘listener’ pada akhir penamaanya.

--- 03-events/events.js
+++ 03-events/events.js
@@ -2,21 +2,39 @@ const { EventEmitter } = require("events");
 
 const myEventEmitter = new EventEmitter();
 
-// fungsi yang akan dijalankan ketika event coffee-order terjadi
-const makeCoffee = ({ name }) => {
+// atau opsi bisa membuat satu fungsi khusus untuk menangani event seperti ini
+const makeCoffee = (name) => {
   console.log(`Kopi ${name} telah dibuat!`);
 };
 
-// tambahkan variabel makeBill dengan const
-const makeBill = ({ price }) => {
+const makeBill = (price) => {
   console.log(`Bill sebesar ${price} telah dibuat!`);
 };
 
-// mendaftarkan fungsi makeCoffe sebagai listener event coffee-order
-myEventEmitter.on("coffee-order", makeCoffee);
+// // fungsi yang akan dijalankan ketika event coffee-order terjadi
+// const makeCoffee = ({ name }) => {
+//   console.log(`Kopi ${name} telah dibuat!`);
+// };
 
-// tambahkan mendaftarkan fungsi makeBill sebagai listener event coffee-order
-myEventEmitter.on("coffee-order", makeBill);
+// // tambahkan variabel makeBill dengan const
+// const makeBill = ({ price }) => {
+//   console.log(`Bill sebesar ${price} telah dibuat!`);
+// };
+
+// // mendaftarkan fungsi makeCoffe sebagai listener event coffee-order
+// myEventEmitter.on("coffee-order", makeCoffee);
+
+// // tambahkan mendaftarkan fungsi makeBill sebagai listener event coffee-order
+// myEventEmitter.on("coffee-order", makeBill);
+
+// atau opsi bisa membuat satu variabel khusus untuk perintah panggilan dalam isi daftar listener
+const onCoffeeOrderedListener = ({ name, price }) => {
+  makeCoffee(name);
+  makeBill(price);
+};
+
+// tambahkan mendaftarkan fungsi onCoffeeOrderListener sebagai listener event coffee-order
+myEventEmitter.on("coffee-order", onCoffeeOrderedListener);
 
 // Memicu event 'coffee-order' terjadi.\
 // tambahkan variabel price dengan nilai 15 ribu
@@ -25,4 +43,5 @@ myEventEmitter.emit("coffee-order", { name: "Tubruk", price: 15000 });
 /*
  * output:
  * Kopi Tubruk telah dibuat!
+ * Bill sebesar 15000 telah dibuat!
  */
